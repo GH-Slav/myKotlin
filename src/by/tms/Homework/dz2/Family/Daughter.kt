2f10c8ea4383671cfe3295cd9bd3404ce8dfd7e2
Slava <59572751+GH-Slav@users.noreply.github.com>
2020-02-14 15:47:41 +0300

    kotlin

--- src/by/tms/Homework/dz2/Family/Daughter.kt
+++ src/by/tms/Homework/dz2/Family/Daughter.kt
@@ -1,14 +1,14 @@
 package by.tms.Homework.dz2.Family
 
-class Daughter(name: String, age: Int, sex: Human.SEX, birthplace: String, hasNameFather: Father) :
-    Mother(name, age, sex, birthplace, hasNameFather) {
+class Daughter(name: String, age: Int, sex: Human.SEX, birthplace: String, hasFather:String) :
+    Mother(name, age, sex, birthplace, hasFather) {
     override fun say() {
 
             println("My name is $name \n " +
                     "I am $age years old \n" +
                     "My gender $sex \n" +
                     "I was born $birthplace \n"+
-                    "I'm married - $hasNameFather")
+                    "I'm married - $hasFather")
 
     }
 }
