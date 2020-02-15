8a93da81ed92aa9d5a90f7aeecd85850e9a4927a
Slava <59572751+GH-Slav@users.noreply.github.com>
2020-02-15 18:33:58 +0300

    Delete SonImpl.kt

--- src/by/tms/Homework/dz2/Family/SonImpl.kt
+++
@@ -1,8 +0,0 @@
-package by.tms.Homework.dz2.Family
-
-class SonImpl(name: String, age: Int, sex: Human.SEX, occupation: String, hasFamily: Boolean) :
-    FatherImpl(name, age, sex, occupation, hasFamily), Son {
-
-    override fun say() =
-      println("My name is $name. \n I am $age years old. \n My gender $sex. \n I was engaged $occupation \n I'm married - $hasFamily \n")
-}
